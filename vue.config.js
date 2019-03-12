d917f817569b60b10ac91d9401f723e4f76d7d11
anttus <anttu.suhonen@gmail.com>
2019-03-12 12:03:08 +0200

    Config fiel

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
-module.exports {
-    devServer: {
-        open: process.platform === 'darwin',
-        host: 'anttus.ddns.net',
-	port: 5000,
-	https: true,
-	hotOnly: false,
-    },
-}
+module.exports = {
+  devServer: {
+    open: process.platform === "darwin",
+    host: "anttus.ddns.net",
+    port: 5000,
+    https: true,
+    hotOnly: false
+  }
+};
